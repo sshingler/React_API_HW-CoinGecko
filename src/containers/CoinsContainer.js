77356abc156a6a21c97c2460e6abb2c5154ec735
SShingler <s.shingler@hotmail.co.uk>
2023-03-11 12:47:53 +0000

    CoinsContainer fetch coinGecko API

--- src/containers/CoinsContainer.js
+++ src/containers/CoinsContainer.js
@@ -1,8 +1,19 @@
-import React from "react"; 
+import React, {useEffect, useState} from "react"; 
 import CoinsList from "../components/CoinsList";
 
 const CoinsContainer = () => {
+    const [allCoins, setAllCoins] = useState ([]);
 
+
+    useEffect (() => {
+        getCoins();
+    }, []);
+
+    const getCoins = function () {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h')
+        .then(res => res.json())
+        .then(allCoins =>setAllCoins(allCoins))
+    }
 
     return (
         <div className="CoinsContainer">       
